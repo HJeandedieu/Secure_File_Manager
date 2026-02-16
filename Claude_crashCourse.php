97d85f87f86ddda554a155465789edbc38000d8e
jd <hashimweyesujeandedie@gmail.com>
2026-02-16 23:04:04 +0200

    Added the file manupulation functionalities

--- Claude_crashCourse.php
+++ Claude_crashCourse.php
@@ -23,4 +23,19 @@ foreach($age as $name => $ageValue){
     echo "Name: $name, Age : $ageValue<br>";
 }
 
+$students = array(
+        array("kabanda", 14, 90),
+        array("uwera", 14, 86),
+        array("Ndagijimana",14, 87)
+);
+
+for( $row = 0; $row < 3; $row++){
+    for($col = 0; $col < 3; $col++){
+        echo $students[$row][$col] . "  ";
+    }echo "<br>";
+}
+/*This prints arrays in a readable format for simple display
+without complexity of data types and length which maybe
+unnecessary in some cases*/
+print_r($students);
 ?>
---
+++ date.php
@@ -0,0 +1,5 @@
+<?php
+//mktime(hour,minute,second,month,day,year);
+$christmas = mktime(0,0,0,12,25,2024);
+
+echo $christmas;
